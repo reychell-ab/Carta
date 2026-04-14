a0744292f8c8a6cad9479dd2cb1e6ce4d4471dca
Reychell Acuña <reychell2255@gmail.com>
2026-04-13 21:30:51 -0600

    Fix Railway port definitivo

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// 🔥 IMPORTANTE
-const PORT = process.env.PORT || 3000;
+// 🔥 SOLO esto (sin fallback)
+const PORT = process.env.PORT;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
